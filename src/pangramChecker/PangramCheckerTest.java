cdd5e61a400f428b83210904a7e114bb477a0af0
Will <37053782+williamDunn@users.noreply.github.com>
2020-11-16 13:04:20 -0600

    Update PangramCheckerTest.java

--- src/pangramChecker/PangramCheckerTest.java
+++ src/pangramChecker/PangramCheckerTest.java
@@ -8,14 +8,12 @@ class PangramCheckerTest {
 	@Test
     public void test1() {
       String pangram1 = "The quick brown fox jumps over the lazy dog";
-      PangramChecker pc = new PangramChecker();
-      assertEquals(true, pc.check(pangram1));
+      assertEquals(true, PangramChecker.check(pangram1));
     }
     @Test
     public void test2() {
       String pangram2 = "not quite";
-      PangramChecker pc = new PangramChecker();
-      assertEquals(false, pc.check(pangram2));
+      assertEquals(false, PangramChecker.check(pangram2));
     }
 
 }
